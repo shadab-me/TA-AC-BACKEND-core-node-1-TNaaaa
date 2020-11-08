c35795393880e484c8710aa44b9cf9ca635ebf10
Shadab Ali <shadabali604@gmail.com>
2020-11-08 11:00:44 +0530

    added: server 4 and 5

--- block-BNaaau/server4.js
+++ block-BNaaau/server4.js
@@ -1,8 +1,8 @@
 let http = require('http');
-const { hrtime } = require('process');
-let server = http.createServer(server1Handler);
-function server1Handler(req, res){
-    console.log(req)
+const { url } = require('inspector');
+ let server = http.createServer(serverHandler);
+function serverHandler(req, res){
+    console.log(req,url, req.method) 
 }
-server.listen(5000, () => console.log('server is running on port 5000'));
+server.listen(5566, () => console.log('server is running on port 5566'));
 
--- block-BNaaau/server5.js
+++ block-BNaaau/server5.js
@@ -1,8 +1,7 @@
 let http = require('http');
-const { hrtime } = require('process');
-let server = http.createServer(server1Handler);
-function server1Handler(req, res){
-    console.log(req)
+ let server = http.createServer(serverHandler);
+function serverHandler(req, res){
+    res.end(req.headers)
 }
-server.listen(5000, () => console.log('server is running on port 5000'));
+server.listen(7000, () => console.log('server is running on port 7000'));
 
